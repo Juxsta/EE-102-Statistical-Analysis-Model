cb294ade706f1fc33343a36ebdc0bebbc78d0bd7
Eric Reyes <ericjrreyes2@gmail.com>
2020-04-04 17:19:33 -0700

    changes

--- Linear_regression.m
+++ Linear_regression.m
@@ -1,12 +1,12 @@
-data = readtable('PPDataSheet.xlsx');
+data = table2cell(readtable('PPDataSheet.xlsx'));
 X = data(:, 1:3);% X = input variables 
 y = data(:, 5); % Y = output variable (PE)
 m = length(y);
-X = [ones(m, 1) X];
+X = [ones(m, 3) X];
 alpha = 0.01;
 num_iters = 400;
  K = zeros(4, 1);
-[K, J_history] = gradientDescentMulti(X, y, K, alpha, num_iters);
+[K, J_history] = gradientDescent(X, y, K, alpha, num_iters);
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
