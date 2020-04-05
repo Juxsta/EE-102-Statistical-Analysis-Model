8515d1e1874e86eaca2a14b5cd693dfc9823e810
ismahascharisma <63170302+ismahascharisma@users.noreply.github.com>
2020-04-04 17:11:49 -0700

    test

--- Linear_regression.m
+++ Linear_regression.m
@@ -1,6 +1,12 @@
 data = readtable('PPDataSheet.xlsx');
-X = data(:, 1:4);% X = input variables 
+X = data(:, 1:3);% X = input variables 
 y = data(:, 5); % Y = output variable (PE)
 m = length(y);
 X = [ones(m, 1) X];
-K = normalEqn(X, y);
+alpha = 0.01;
+num_iters = 400;
+ K = zeros(4, 1);
+[K, J_history] = gradientDescentMulti(X, y, K, alpha, num_iters);
+plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+xlabel('Number of iterations');
+ylabel('Cost J');
